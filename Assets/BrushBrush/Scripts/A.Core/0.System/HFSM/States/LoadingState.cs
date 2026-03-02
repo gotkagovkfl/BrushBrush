b7f541bc62c2e27ce13b20e7bb071efb8709a529
김중현 <gotka0211@gmail.com>
2026-03-02 23:25:16 +0900

    [ HFSM ] 상태 전이 요청시 로딩 상태 경유하는 구조 설계

--- Assets/BrushBrush/Scripts/A.Core/0.System/HFSM/States/LoadingState.cs
+++ Assets/BrushBrush/Scripts/A.Core/0.System/HFSM/States/LoadingState.cs
@@ -1,11 +1,39 @@
+using System;
+using System.Data;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 
+/// <summary>
+/// 전환될 씬의 파라미터
+/// </summary>
 public readonly struct LoadingPayload : IStatePayload<LoadingState>
 {
+    public readonly Type NextStateType;
+    public readonly Action Invoke;
+
+    LoadingPayload(Type stateType, Action invoke)
+    {
+        NextStateType = stateType;
+        Invoke = invoke;
+    }
     
+    /// <summary>
+    /// 외부에서 상태 전환 함수를 직접 인자에 넣는 방식이 마음에 안들어서 이렇게 수정
+    /// </summary>
+    public static LoadingPayload Create<TState>(IStatePayload<TState> payload) where TState : IState
+    {
+        return new LoadingPayload(
+            typeof(TState),
+            () => GameStateManager.Instance.Request<TState>(payload)
+        );
+    }
+
 }
 
+/// <summary>
+/// 로딩 상태 - 씬을 불러오며, 기존 씬의 리소스를 해제하고, 새로운 씬의 리소스를 불러오고 세팅한다. 
+/// 그동안은 로딩 이미지를 표시한다.
+/// </summary>
 public class LoadingState : BaseGameState<LoadingPayload>
 {
     protected override void Enter_Impl()
@@ -22,4 +50,7 @@ public class LoadingState : BaseGameState<LoadingPayload>
     {
         
     }
+
+    //=============================================================
+
 }
--- Assets/BrushBrush/Scripts/A.Core/1.Managers/GameStateManager.cs
+++ Assets/BrushBrush/Scripts/A.Core/1.Managers/GameStateManager.cs
@@ -56,6 +56,8 @@ public class GameStateManager : MonoSingleton<GameStateManager>
     /// </summary>
     public void SwitchMainStateTo<TState>(IStatePayload<TState> payload) where TState : IState
     {
+        LoadingPayload loadingPayload = LoadingPayload.Create<TState>(payload);
+        Request<LoadingState>(loadingPayload);
     }
 
     /// <summary>
