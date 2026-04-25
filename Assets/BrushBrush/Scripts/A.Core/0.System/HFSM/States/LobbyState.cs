1c8a280c1c706145de67bf467a52b409eab11cb6
김중현 <gotka0211@gmail.com>
2026-04-26 03:14:07 +0900

    [ 테스트 ] 타이틀 -> 로비 -> 인게임 으로 바로 전환할 수 있는 테스트 코드 추가

--- Assets/BrushBrush/Scripts/A.Core/0.System/HFSM/States/LobbyState.cs
+++ Assets/BrushBrush/Scripts/A.Core/0.System/HFSM/States/LobbyState.cs
@@ -3,30 +3,52 @@ using Cysharp.Threading.Tasks;
 
 public struct LobbyPayload : IStatePayload<LobbyState>
 {
-    
+
 }
 
 
-public class LobbyState: BaseGameState<LobbyPayload>
+public class LobbyState : BaseGameState<LobbyPayload>
 {
     protected override async UniTask Enter_Impl()
     {
-        
+
     }
 
-    protected override void StartState()
+    protected override void StartState_Impl()
     {
-        
+        StartGame();
     }
 
 
     protected override async UniTask Exit_Impl()
     {
-        
+
     }
 
     protected override void Update_Impl()
     {
-        
+
+    }
+
+    //=========================================================
+    /// <summary>
+    /// 현재 설정대로 게임을 플레이 한다.
+    /// </summary>
+    void StartGame()
+    {
+        InGamePayload payload = CreateInGamePayload();
+        GameStateManager.Instance.SwitchMainStateTo<InGameState>(payload);
+    }
+    
+    /// <summary>
+    /// 현재 세팅에 맞춰 인게임 페이로드 생성
+    /// </summary>
+    InGamePayload CreateInGamePayload()
+    {
+        return new InGamePayload
+        { 
+            StageId = "Test",
+            CharacterId = "Test",
+        };
     }
 }
--- Assets/BrushBrush/Scripts/A.Core/0.System/HFSM/States/TitleState.cs
+++ Assets/BrushBrush/Scripts/A.Core/0.System/HFSM/States/TitleState.cs
@@ -14,9 +14,9 @@ public class TitleState: BaseGameState<TitlePayload>
 
     }
 
-    protected override void StartState()
+    protected override void StartState_Impl()
     {
-        
+        Test().Forget();
     }
 
     protected override async UniTask Exit_Impl()
@@ -27,5 +27,26 @@ public class TitleState: BaseGameState<TitlePayload>
     protected override void Update_Impl()
     {
         
+    }
+
+
+    //=================================
+    
+    /// <summary>
+    /// 테스트니까 이거하위 상태로 쪼개서 그 안에 넣어야함 (데이터로드 스테이트)
+    /// </summary>
+    async UniTask Test()
+    {
+        await BBData.InitStreamingData();
+        GoLobby();
+    }
+
+    /// <summary>
+    /// 현재 설정대로 게임을 플레이 한다.
+    /// </summary>
+    void GoLobby()
+    {
+        LobbyPayload payload = new();
+        GameStateManager.Instance.SwitchMainStateTo<LobbyState>(payload);
     }
 }
